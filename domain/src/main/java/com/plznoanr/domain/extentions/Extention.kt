8f5511e67266924aa6544a0bf11810300bdd92f5
sg.park <psgxxx@nm-metaworld.com>
2023-03-22 18:59:53 +0900

    1. BaseUseCase - Add Log

--- domain/src/main/java/com/plznoanr/domain/extentions/Extention.kt
+++ domain/src/main/java/com/plznoanr/domain/extentions/Extention.kt
@@ -4,3 +4,25 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
 fun <T>Result<T>.asFlow(): Flow<Result<T>> = flowOf(this)
+
+fun String.log() {
+    decoration("LOG")
+    this
+        .split("/")
+        .forEachIndexed { index, s ->
+            when (index) {
+                0 -> println("UseCaseName: $s")
+                1 -> println("Parameter: $s")
+                2 -> println("Result: $s")
+            }
+        }
+    decoration("END")
+}
+
+private fun decoration(msg: String) = run {
+    print(msg)
+    for (i in 0..100) {
+        print("-")
+    }
+    println()
+}
