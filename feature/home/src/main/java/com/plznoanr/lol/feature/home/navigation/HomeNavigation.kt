ca3310ad6f910adc17cf162fcb4364bd60dffa9f
sg.park <psgxxx@nm-metaworld.com>
2024-03-19 15:12:39 +0900

    [리팩토링] - 코드 수정

--- feature/home/src/main/java/com/plznoanr/lol/feature/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/java/com/plznoanr/lol/feature/home/navigation/HomeNavigation.kt
@@ -1,17 +1,11 @@
 package com.plznoanr.lol.feature.home.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.plznoanr.lol.feature.home.HomeRoute
 import kotlinx.coroutines.flow.Flow
 
 const val HomeRoute = "home_route"
-
-fun NavController.navigateToHome(navOptions: NavOptions? = null) {
-    navigate(HomeRoute, navOptions)
-}
 
 fun NavGraphBuilder.homeScreen(
     navCallbackFlow: Flow<Boolean>
--- feature/setting/src/main/java/com/plznoanr/lol/feature/setting/navigation/SettingNavigation.kt
+++ feature/setting/src/main/java/com/plznoanr/lol/feature/setting/navigation/SettingNavigation.kt
@@ -1,16 +1,10 @@
 package com.plznoanr.lol.feature.setting.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.plznoanr.lol.feature.setting.SettingRoute
 
 const val SettingRoute = "setting_route"
-
-fun NavController.navigateToSetting(navOptions: NavOptions? = null) {
-    navigate(SettingRoute, navOptions)
-}
 
 fun NavGraphBuilder.settingScreen(
     onShowSnackbar: suspend (String) -> Boolean
